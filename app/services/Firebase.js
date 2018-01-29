60d97b3b9afcc97f1bdaaf5c51e13b41a93de726
Mads Pedersen <mads.p@icloud.com>
2018-01-29 21:14:47 +0100

    Update Firebase.js

--- app/services/Firebase.js
+++ app/services/Firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 var config = {
-  apiKey: "AIzaSyCDHBu-mBTM60-AP30Vwu9rrP6Q4Ar9ujQ",
-  authDomain: "discoveridge-ae18e.firebaseapp.com",
-  databaseURL: "https://discoveridge-ae18e.firebaseio.com",
-  projectId: "discoveridge-ae18e",
-  storageBucket: "discoveridge-ae18e.appspot.com",
-  messagingSenderId: "54894293236"
+  apiKey: "**********************'",
+  authDomain: "**********************",
+  databaseURL: "**********************",
+  projectId: "**********************",
+  storageBucket: "**********************",
+  messagingSenderId: "**********************"
 }
 
 export const firebaseRef = firebase.initializeApp(config)
